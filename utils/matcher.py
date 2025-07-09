b761e7de62a8eae833107b6e35cbff84d0f80b06
pritt18 <140495870+pritt18@users.noreply.github.com>
2025-07-09 13:47:54 +0530

    Add files via upload

---
+++ utils/matcher.py
@@ -0,0 +1,22 @@
+from scipy.spatial.distance import cdist
+import numpy as np
+
+def match_players(boxes_broadcast, boxes_tacticam):
+    """
+    Match players detected in broadcast and tacticam views using IoU or center-point distance.
+    Returns a dictionary mapping tacticam index to broadcast index (ID).
+    """
+    centers_b = np.array([[((box[0] + box[2]) / 2), ((box[1] + box[3]) / 2)] for box in boxes_broadcast])
+    centers_t = np.array([[((box[0] + box[2]) / 2), ((box[1] + box[3]) / 2)] for box in boxes_tacticam])
+
+    if len(centers_b) == 0 or len(centers_t) == 0:
+        return {}
+
+    dist_matrix = cdist(centers_t, centers_b)  
+
+    matched = {}
+    for i, row in enumerate(dist_matrix):
+        min_idx = np.argmin(row)
+        matched[i] = min_idx
+
+    return matched
